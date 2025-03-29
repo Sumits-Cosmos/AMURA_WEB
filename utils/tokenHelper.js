aee031344f2e9099eb2b405039f6e8314b36a482
aadityaraj11 <aadityarajgiri11@gmail.com>
2025-03-29 14:13:17 +0530

    Delete utils directory

--- utils/tokenHelper.js
+++
@@ -1,8 +0,0 @@
-// utils/tokenHelper.js
-module.exports.extractToken = (req) => {
-    let token = req.cookies?.token;
-    if (!token && req.headers.authorization?.startsWith('Bearer ')) {
-        token = req.headers.authorization.split(' ')[1];
-    }
-    return token;
-};
