91f90b7e4d4d9769a16230fbbc7d908f28d3fd02
aadityaraj11 <aadityarajgiri11@gmail.com>
2025-03-29 14:11:54 +0530

    Delete models directory

--- models/blackListToken.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const blackListTokenSchema = new mongoose.Schema({
-  token: { type: String, required: true },
-  createdAt: { type: Date, default: Date.now, expires: '1d' }
-});
-
-module.exports = mongoose.model('BlackListToken', blackListTokenSchema);
